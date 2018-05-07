266ae834d11b8c806ca8efdabe38e6670deb473c
Xonij <joni.sil94@gmail.com>
2018-05-07 10:47:48 +0300

    sound manager, 2 scripts, cave & airship area sounds, gym sounds (triggers).

---
+++ Assets/Scriptit/scripts - audio/musicSender.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicSender : MonoBehaviour {
+
+    [Range(0,10)]
+    public int audioClipToPlay;
+
+	void Start () {
+		
+	}
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SoundManager.instance.musicSource.clip = SoundManager.instance.inGameAudioCLips[audioClipToPlay];
+            SoundManager.instance.musicSource.Play();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SoundManager.instance.musicSource.clip = null;
+        }
+    }
+}
